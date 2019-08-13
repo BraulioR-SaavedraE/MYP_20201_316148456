257b6664e7cad2e18d192e226b175386816db6c2
Braulio Rubén Savedra Escalona <braulio@ciencias.unam.mx>
2019-08-13 13:02:18 -0500

    Agregando primer ejercicio en C

---
+++ Primo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int esPrimo(int a);
+
+int main()
+{
+	printf("%s\n", "Introduce un número:");
+	int t;
+	scanf("%i", &t);
+	printf("%s%i\n", "Introdujiste: ", t);
+	printf("%s\n", "Los números primos menores a él, son: ");
+
+	int y;
+	for(y = t; y >= 1; y--)
+		if(esPrimo(y))
+			printf("%i\n", y);
+}
+
+int esPrimo(int a)
+{
+	int x;
+
+	for(x = a / 2; x >= 2; x--)
+		if((a % x) == 0)
+			return 0;
+
+	return 1;
+}
