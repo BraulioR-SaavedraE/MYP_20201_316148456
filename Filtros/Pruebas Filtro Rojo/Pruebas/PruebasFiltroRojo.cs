abd7761509349358087ef2526b74c98b22acd9c4
Braulio Rubén Saavedra Escalona <braulio@ciencias.unam.mx>
2019-09-04 21:47:47 -0500

    Agregando esqueleto de pruebas unitarias

---
+++ Filtros/Pruebas Filtro Rojo/Pruebas/PruebasFiltroRojo.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PruebasRojas
+{
+    public class TestRojo
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
