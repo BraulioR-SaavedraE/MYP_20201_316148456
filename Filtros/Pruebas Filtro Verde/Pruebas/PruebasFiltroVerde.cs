498adbe35de79e340ed835cb654ab9af5b5e2380
Braulio Rubén Saavedra Escalona <braulio@ciencias.unam.mx>
2019-09-07 21:32:46 -0500

    Agregando pruebas unitarias para el filtro verde

--- Filtros/Pruebas Filtro Verde/Pruebas/PruebasFiltroVerde.cs
+++ Filtros/Pruebas Filtro Verde/Pruebas/PruebasFiltroVerde.cs
@@ -1,17 +1,33 @@
 using NUnit.Framework;
+using System.Drawing;
+using Filtros.Programa.Aplicación;
 
 namespace PruebasVerdes
 {
     public class TestVerde
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void RojoYAzulCero()
         {
+            
         }
 
         [Test]
         public void Test1()
         {
+            FiltroVerde filtro = new FiltroVerde();
+            Bitmap imagen = filtro.Copia();
+            filtro.AplicaFiltro(imagen);
+
+            for (int i = 0; i < imagen.Width; i++)
+            {
+                for (int j = 0; j < imagen.Height; j++)
+                {
+                    Color pixelColor = imagen.GetPixel(i, j);
+                    if (pixelColor.R != 0 || pixelColor.B != 0)
+                        Assert.Fail();
+                }
+            }
             Assert.Pass();
         }
     }
