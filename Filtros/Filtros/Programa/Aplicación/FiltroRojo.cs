5270820c2b0ad6aef9a4022e2ed211cffcf1b429
Braulio Rubén Saavedra Escalona <braulio@ciencias.unam.mx>
2019-09-07 23:06:24 -0500

    Agregando Filtro Rojo y Mosaico

---
+++ Filtros/Filtros/Programa/Aplicación/FiltroRojo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Filtros.Programa.Aplicación
+{
+    public class FiltroRojo : Filtros
+    {
+
+        override
+        public void AplicaFiltro(Bitmap imagen)
+        {
+
+            for (int x = 0; x < imagen.Width; x++)
+            {
+                for (int y = 0; y < imagen.Height; y++)
+                {
+
+                    Color pixelColor = imagen.GetPixel(x, y);
+                    //if(pixelColor.R == 0)
+                    //  throw new ArgumentException();
+
+                    Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                    imagen.SetPixel(x, y, newColor);
+                }
+            }            
+        }
+    }
+}
