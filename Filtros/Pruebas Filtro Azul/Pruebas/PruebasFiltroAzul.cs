9c778dafe894f682d12eff1b665f67681369a85d
Braulio Rubén Saavedra Escalona <braulio@ciencias.unam.mx>
2019-09-07 21:33:33 -0500

    Agregando pruebas unitarias para el filtro azul

--- Filtros/Pruebas Filtro Azul/Pruebas/PruebasFiltroAzul.cs
+++ Filtros/Pruebas Filtro Azul/Pruebas/PruebasFiltroAzul.cs
@@ -1,17 +1,27 @@
 using NUnit.Framework;
+using System.Drawing;
 
 namespace PruebasAzules
 {
     public class TestAzul
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
 
         [Test]
-        public void Test1()
+        public void RojoYVerdeCero
         {
+            FiltroAzul filtro = new FiltroAzul();
+            Bitmap imagen = filtro.Copia();
+            filtro.AplicaFiltro(imagen);
+
+            for (int i = 0; i < imagen.Width; i++)
+            {
+                for (int j = 0; j < imagen.Height; j++)
+                {
+                    Color pixelColor = imagen.GetPixel(i, j);
+                    if (pixelColor.R != 0 || pixelColor.G != 0)
+                        Assert.Fail();
+                }
+            }
             Assert.Pass();
         }
     }
