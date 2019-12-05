c879448442a1605f9e0d588f6075e0717c140d39
BraulioR-SaavedraE <braulio@ciencias.unam.mx>
2019-12-04 23:53:41 -0600

    Agregando HTML

---
+++ Proyecto03/Contador de Palabras/src/IndexGenerador.kt
@@ -0,0 +1,50 @@
+import java.io.*
+import java.util.LinkedList
+
+class IndexGenerador(private val nombres: LinkedList<String>) {
+
+    fun generaIndex(directorio: String) {
+        var index = HEADER
+
+        for (x in nombres) {
+            val nombre = x.substring(x.lastIndexOf("/") + 1, x.length())
+            index += REF
+            index = index.replaceFirst("ARCHIVO", x + ".html")
+            index = index.replaceFirst("NOMBRE", nombre)
+        }
+
+        index += FOOTER
+
+        guardaHTML(directorio, "index", index)
+    }
+
+    companion object {
+        private val HEADER = ("<!DOCTYPE html>\n" + "<html>\n" + "<head>\n"
+                + "<title>Index</title>\n"
+                + "<meta charset=\"UTF-8\">")
+        private val FOOTER = "</html>"
+        private val REF = "<a href=\"ARCHIVO\">NOMBRE</a>"
+
+        private fun guardaHTML(directorio: String, nombre: String, html: String) {
+            val path = "$directorio/$nombre.html"
+            try {
+                val myObj = File(path)
+                myObj.createNewFile()
+
+            } catch (e: IOException) {
+                System.out.println(e)
+            }
+
+            try {
+                val myWriter = FileWriter(path)
+                myWriter.write(html)
+                myWriter.close()
+            } catch (e: IOException) {
+                System.out.println(e)
+            }
+
+        }
+    }
+
+
+}
